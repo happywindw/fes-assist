3ed92754990e255b497e62efef9d690ae55b00a4
happywindw <592595468@qq.com>
2018-05-04 19:39:07 +0800

    add request classes

--- fesrequests.py
+++ fesrequests.py
@@ -10,3 +10,22 @@ data = {
 
 rep = requests.post('http://10.200.24.47:6080/fes-service/busi-api/2301', data=json.dumps(data))
 print(rep.text)
+
+
+class FesRequest(object):
+    def __init__(self, trans_code):
+        self.trans_code = trans_code
+
+    def post(self):
+        pass
+
+
+class FesB2301(FesRequest):
+    def __init__(self, trans_code, check_date, org_nick_name):
+        super().__init__(trans_code)
+        self.check_date = check_date
+        self.org_nick_name = org_nick_name
+
+    def post(self):
+        print('ss')
+        pass
