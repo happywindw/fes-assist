0a6536bc7bf24291e14b41d289b7f34c16bc4e03
happywindw <592595468@qq.com>
2018-05-07 12:51:29 +0800

    add fesbusi class

--- dbapi.py
+++ dbapi.py
@@ -16,7 +16,7 @@ class DataBaseApi(object):
     def __init__(self):
         self.session = self.DBSession()
 
-    def account_check(self, settle_type, tran_date, is_check, org_nick_name):
+    def account_check(self, tran_date, settle_type, is_check, org_nick_name):
         res = self.session.execute("select tran_date, count(1), sum(total_amt) from fes_online_detail where "
                                    "settle_type='%s' and tran_date='%s' and is_check='%s' and org_nick_name='%s' "
                                    "group by tran_date" % (settle_type, tran_date, is_check, org_nick_name))
---
+++ fesbusi.py
@@ -0,0 +1,18 @@
+from dbapi import DataBaseApi
+
+
+class FesBusi(object):
+    def __init__(self):
+        self.db = DataBaseApi()
+
+    def check_account(self, tran_date, settle_type, org_nick_name):
+        res = self.db.account_check(tran_date, settle_type, 0, org_nick_name)
+        if res:  # 未对账
+            pass
+        else:
+            res = self.db.account_check(tran_date, settle_type, 1, org_nick_name)
+            if res:  # 已对账
+                pass
+            else:    # 无业务
+                pass
+
