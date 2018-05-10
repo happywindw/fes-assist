cfd64f3b94ff4a159c82aaf15b8cac69692f69bc
happywindw <592595468@qq.com>
2018-05-10 19:37:45 +0800

    add a method to insert data to grid table

--- fesui.py
+++ fesui.py
@@ -134,21 +134,33 @@ class FesRootFrame(RootFrame):
         event.Skip()
 
     @staticmethod
-    def insert_into_gird(grid, data):
+    def insert_into_gird(grid, data, row_labels=None, col_labels=None):
         if not len(data):
             return
-        row_num, column_num = len(data), len(data[0])
+        row_num, col_num = len(data), len(data[0])
         grid_row_num = grid.GetNumberRows()
-        print(grid.GetNumberRows())
+        grid_col_num = grid.GetNumberCols()
+
+        # 设置行数与列数使之与数据匹配
         if row_num > grid_row_num:
             grid.AppendRows(row_num - grid_row_num)
         if row_num < grid_row_num:
-            grid.DeleteRows(1, grid_row_num - row_num)
-        i, j = 0, 0
-        for row in data:
-            for ele in row:
+            grid.DeleteRows(row_num, grid_row_num - row_num)
+        if col_num > grid_col_num:
+            grid.AppendCols(col_num - grid_col_num)
+        if col_num < grid_col_num:
+            grid.DeleteCols(col_num, grid_col_num - col_num)
+
+        # 设置各行、列的标题
+        if row_labels and len(row_labels) == row_num:
+            for i, lab in enumerate(row_labels):
+                grid.SetRowLabelValue(i, lab)
+        if col_labels and len(col_labels) == col_num:
+            for j, lab in enumerate(col_labels):
+                grid.SetColLabelValue(j, lab)
+
+        # 填充数据
+        for j, row in enumerate(data):
+            for i, ele in enumerate(row):
                 grid.SetCellValue(j, i, str(ele))
-                i += 1
-            i = 0
-            j += 1
         grid.AutoSize()
